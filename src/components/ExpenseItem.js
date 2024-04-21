2d7ea7a5523ac556e0bbd49edda90aa8b52dba9f
Hesham Mohammed Noaman <140953153+HeshamNoaman@users.noreply.github.com>
2024-04-21 12:08:48 +0300

    Shopping Application lab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,35 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { FaTimesCircle } from 'react-icons/fa';
+import { type } from '@testing-library/user-event/dist/type';
 
 const ExpenseItem = (props) => {
+    const {dispatch, Location} = useContext(AppContext);
+
+    const handelDeleteItem = () => {
+        const item = {
+            name: props.name
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item
+        })
+
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.quantity}</td>
+            <td>{Location}{parseInt(props.unitprice)}</td>
+            <td>{Location}{parseInt(props.quantity) * parseInt(props.unitprice)}</td>
+            <td><FaTimesCircle size={'2.2em'} color='red' onClick={handelDeleteItem} ></FaTimesCircle></td>
+
+
+        </tr>
+
+    )
 };
 
 export default ExpenseItem;
